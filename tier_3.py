3c609cbb0668d90fb7a4a98b7f3f7e77e161363c
consultantds-maker <consultantds@saicjs.com>
2026-02-24 16:36:32 +0530

    Update tier_3.py

--- tier_3.py
+++ tier_3.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # -----------------------
 # Load data
 # -----------------------
-df = pd.read_csv(r"E:\s\Tier3_clean.csv", encoding="latin1")
+df = pd.read_csv(r"Tier3_clean1.csv", encoding="latin1")
 df.columns = df.columns.str.strip()  # Remove extra spaces
 
 # -----------------------
@@ -106,3 +106,4 @@ else:
 
     st.plotly_chart(fig, use_container_width=True)
     st.write(metric["chart_desc"])
+
