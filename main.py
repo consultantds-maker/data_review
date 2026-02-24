286a612c7ff6ca7bb3812735e0cca0bd49629e50
consultantds-maker <consultantds@saicjs.com>
2026-02-24 17:56:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ main_page = st.Page("main.py", title="Main Page", icon="🎈")
 # Define the pages
 main_page = st.Page(r"main.py", title="Main Page", icon="🎈")
 page_1 = st.Page(r"Suraksha_Lens.py", title="Discover Suraksha Lens with us", icon="❄️")
-page_5 = st.Page(r"tier_1.py", title="Hazard", icon="❄️")
-page_2 = st.Page(r"tier_2.py", title="Exposure", icon="❄️")
-page_3 = st.Page(r"tier_3.py", title="Vulnerability", icon="❄️")
-page_4 = st.Page(r"tier_4.py", title="Ceri", icon="❄️")
+page_5 = st.Page(r"tier_1.py", title=" Tier 1: Hazard", icon="❄️")
+page_2 = st.Page(r"tier_2.py", title="Tier 2: Exposure", icon="❄️")
+page_3 = st.Page(r"tier_3.py", title="Tier 3: Vulnerability", icon="❄️")
+page_4 = st.Page(r"tier_4.py", title="Tier 4: Ceri", icon="❄️")
 
 # Set up navigation
 pg = st.navigation([page_1,page_5,page_2,page_3,page_4])
 
 # Run the selected page
 pg.run()
+
 
 
 
