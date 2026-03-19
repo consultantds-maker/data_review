3a9c37d3c95ee782104213f7b973f14b51434389
consultantds-maker <consultantds@saicjs.com>
2026-03-19 19:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ main_page = st.Page("main.py", title="Main Page", icon="🎈")
 # Define the pages
 main_page = st.Page(r"main.py", title="Main Page", icon="🎈")
 page_1 = st.Page(r"Suraksha_Lens.py", title="Discover Suraksha Lens with us", icon="❄️")
-page_5 = st.Page(r"tier1_india_srilanka.py", title=" Tier 1: Hazard", icon="❄️")
+page_5 = st.Page(r"tier_1.py", title=" Tier 1: Hazard", icon="❄️")
 page_2 = st.Page(r"tier_2.py", title="Tier 2: Exposure", icon="❄️")
 page_3 = st.Page(r"tier_3.py", title="Tier 3: Vulnerability", icon="❄️")
 page_4 = st.Page(r"tier_4.py", title="Tier 4: Ceri", icon="❄️")
