26dddef74ac144dba2dcf9b41fbd195ff6d03d90
consultantds-maker <consultantds@saicjs.com>
2026-04-11 21:32:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ page_1 = st.Page(r"Suraksha_Lens.py", title="Discover Suraksha Lens with us", ic
 page_5 = st.Page(r"tier1_india_srilanka_copy_2.py", title=" Tier 1: Hazard", icon="❄️")
 page_2 = st.Page(r"tier2_india_srilanka.py", title="Tier 2: Exposure", icon="❄️")
 page_3 = st.Page(r"tier3_india_srilanka.py", title="Tier 3: Vulnerability", icon="❄️")
-page_4 = st.Page(r"tier4_india_srilanka.py", title="Tier 4: Ceri", icon="❄️")
+page_4 = st.Page(r"tier4_india_srilanka.py", title="Tier 4: Climate Exploitation Risk Index", icon="❄️")
 
 # Set up navigation
 pg = st.navigation([page_1,page_5,page_2,page_3,page_4])
